ad790a5e2dd5e89602a9b4655b6a7348317c7342
DEVENDRA-5470 <ysdevn@gmail.com>
2026-06-22 11:29:53 +0530

    practice-q2

--- python-class-practice/string_practice.py
+++ python-class-practice/string_practice.py
@@ -1,2 +1,12 @@
-for i in range(1,10):
-    print(i)
+# Q1.
+# for i in range(1,10):
+#     print(i)
+
+# Q2.
+# wap to check string len without len() method
+str1="python"
+c=0
+for i in str1:
+    c+=1
+print(c)
+
